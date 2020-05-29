9bc1bff15b0e566a8d03b402c523582843ace072
nbaugh1 <nbaugh1@gmail.com>
2020-05-29 13:41:35 -0400

    create dummy item

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :items
   devise_for :admin_users, ActiveAdmin::Devise.config
   root to: 'admin/dashboard#index'
   scope '/api' do
