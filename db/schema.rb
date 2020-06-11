35d7d626867547f9fa35eb2ab7a0d4f44132e34b
nbaugh1 <nbaugh1@gmail.com>
2020-06-11 17:54:30 -0400

    scaffold team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_211718) do
+ActiveRecord::Schema.define(version: 2020_06_11_215228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 2020_06_11_211718) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_teams_on_project_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -65,4 +73,5 @@ ActiveRecord::Schema.define(version: 2020_06_11_211718) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "teams", "projects"
 end
